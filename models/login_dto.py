b07e9f4caeb574850607e8f69f144ca5c675d342
Yash <yashinator99@gmail.com>
2022-06-06 14:28:39 -0400

    change the login_dto

--- models/login_dto.py
+++ models/login_dto.py
@@ -1,11 +1,12 @@
 class Login:
-    def __init__(self, user_id, username, password):
+    def __init__(self, user_id, username, password, role):
         self.user_id = user_id
         self.username = username
         self.password = password
+        self.role = role
 
     def __repr__(self) -> str:
-        return f"User Login : id - {self.user_id} username - {self.username} password - {self.password}"
+        return f"User Login : id - {self.user_id} username - {self.username} password - {self.password} role - {self.role}"
 
     def validate_login(self) -> bool:
         if len(self.username) < 5 or len(self.password) < 5:

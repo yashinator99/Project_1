423687970ad2aa736599d1e4a1c6c0ac9a182659
Yash <yashinator99@gmail.com>
2022-06-08 15:38:53 -0400

    fixing employees pages

--- controller/request_controller.py
+++ controller/request_controller.py
@@ -21,7 +21,7 @@ def request_user_input(username,register_input):
         return render_template("failed_request.html")
 
 def cancel_request(request_id):
-    update_status(request_id)
+    update_status(request_id,"cancelled")
     user_id = get_user_id_from_request_id(request_id)
     reequestinfo = get_view_request(user_id)
     return render_template("employee_view_request.html", info=reequestinfo)

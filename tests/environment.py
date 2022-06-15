87251b9d79620177e1bd9d65d88effc3ab7bf79d
Emmanuel Oti <otiemmanuel3710@gmail.com>
2022-06-15 15:10:21 -0400

    added files for automation tests, POM,features,steps,models behave,Drivers. Also added a scenario for employee login

---
+++ tests/environment.py
@@ -0,0 +1,15 @@
+from behave.runner import Context
+from selenium import webdriver
+from POM.homepage import Homepage
+
+def before_all(context: Context):
+    # We need to add a driver to the context
+    context.driver = webdriver.Chrome("Drivers/chromedriver.exe")
+    # We need to add all POMS to the context
+    context.homepage = Homepage(context.driver)
+    # We add an implicit wait to work with latency issues
+    context.driver.implicitly_wait(1)
+
+def after_all(context: Context):
+    # This will make sure at the end of a behave test all the drivers are closed
+    context.driver.quit()

4aa5d239e29d1e24d6f03f692a8e571281863b48
Yash <yashinator99@gmail.com>
2022-06-16 18:36:36 -0400

    adding more feature, steps, and pom

--- POM/homepage.py
+++ POM/homepage.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class Homepage:
     def __init__(self, driver: WebDriver):
         self.driver = driver
-        
+
 
     def employee_login_link(self):
         return self.driver.find_element(By.ID, "employee_login_link")
